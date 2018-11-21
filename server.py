8773c320d8a476ddf2d970432e57d32d8d238909
Javier Benavente Heras <javierbh@alpha04.aulas.gsyc.urjc.es>
2018-11-21 19:27:39 +0100

    server por comandos working

--- server.py
+++ server.py
@@ -3,22 +3,33 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import os
+import sys
 import socketserver
 
+if len(sys.argv) != 4:
+        sys.exit('Usage: python3 server.py IP port audio_file')
+else:
+    if os.path.exists(sys.argv[3]):
+        audio_file = sys.argv[3]
+    else:
+        sys.exit('File not found')
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class RTPHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 100 Trying, SIP/2.0 180 Ringing y SIP/2.0 200 Ol")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
+
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
@@ -26,6 +37,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer((SERVER, 6001), RTPHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
